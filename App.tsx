4a10886eebc96cc20c0fb8dc48f7e654288b4027
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2022-10-18 12:16:39 +0300

    style, es6

--- App.tsx
+++ App.tsx
@@ -4,10 +4,10 @@ import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import { Navigation } from './src/components/Navigation';
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <Navigation />
-    </Provider>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Navigation />
+  </Provider>
+);
+
+export default App;
